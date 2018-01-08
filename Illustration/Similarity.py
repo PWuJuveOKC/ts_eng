79ab7d04ef6b8da93eda92b2eb717f22f1874f18
Peng <pwusff@gmail.com>
2018-01-08 15:19:11 -0500

    Added Similarity outputs

--- Illustration/Similarity.py
+++ Illustration/Similarity.py
@@ -9,8 +9,8 @@ from sklearn.preprocessing import scale
 from sklearn.metrics.pairwise import euclidean_distances
 import seaborn as sns; sns.set()
 
-files = os.listdir("../Data/All_time_series_website/Web_Time_Series_Files_7")
-path = "../Data/All_time_series_website/Web_Time_Series_Files_7"
+files = os.listdir("Data/All_time_series_website/Web_Time_Series_Files_7")
+path = "Data/All_time_series_website/Web_Time_Series_Files_7"
 
 files_ECG = [f for f in files if 'ECG' in f] #456
 files_ART = [f for f in files if 'ART' in f] #132
@@ -39,22 +39,22 @@ def FastDTW(x,y):
     distanceDTW, mypath = fastdtw(x, y, dist=euclidean)
     return distanceDTW
 
+
+dist_mat2 = np.zeros((size*4, size*4))
+start = time.time()
+for i in xrange(size*4):
+    for j in xrange(size*4):
+        lati= AllTS[i]
+        latj =AllTS[j]
+        dist_mat2[i, j] = FastDTW(lati,latj)
+        dist_mat2[j, i] = dist_mat2[i, j]
+print(time.time()-start)
 #
-# dist_mat2 = np.zeros((size*4, size*4))
-# start = time.time()
-# for i in xrange(size*4):
-#     for j in xrange(size*4):
-#         lati= AllTS[i]
-#         latj =AllTS[j]
-#         dist_mat2[i, j] = FastDTW(lati,latj)
-#         dist_mat2[j, i] = dist_mat2[i, j]
-# print(time.time()-start)
-# #
-# #
 #
-# dist_mat2_pd = pd.DataFrame(dist_mat2)
-# dist_mat2_pd.to_csv('../Output/dist_dtw_temp.csv',header=None,index=None)
-#
+
+dist_mat2_pd = pd.DataFrame(dist_mat2)
+dist_mat2_pd.to_csv('Output/dist_dtw_temp.csv',header=None,index=None)
+
 #
 ### Visualization
 def distance_matrix(df,metric):
@@ -63,7 +63,7 @@ def distance_matrix(df,metric):
 
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
-    cmap = cm.get_cmap('rainbow', 30)
+    cmap = cm.get_cmap('jet', 30)
     cax = ax1.imshow(df, interpolation="nearest", cmap=cmap)
     ax1.grid(False)
     #plt.title(metric+' Metric')
@@ -76,6 +76,6 @@ def distance_matrix(df,metric):
     plt.show()
 
 distance_matrix(dist_mat, 'Euclidean')
-plt.savefig("../Output/dist_EU.jpg",dpi=900)
-# distance_matrix(dist_mat2, 'DTW')
-# plt.savefig("../Output/dist_DTW.jpg",dpi=900)
+plt.savefig("Output/dist_EU.pdf",dpi=900)
+distance_matrix(dist_mat2, 'DTW')
+plt.savefig("Output/dist_DTW.pdf",dpi=900)
