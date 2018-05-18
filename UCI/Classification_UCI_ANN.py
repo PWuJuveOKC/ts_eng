a1c2d071ec227eb4858b56d7f17fe401cc3b9b86
Peng <pwusff@gmail.com>
2018-05-17 22:57:55 -0400

    Added UCR Heatmap

--- UCI/Classification_UCI_ANN.py
+++ UCI/Classification_UCI_ANN.py
@@ -11,20 +11,6 @@ num_cores = multiprocessing.cpu_count()
 file_name_train_list = os.listdir("../Data/feat_train")
 file_name_test_list = os.listdir("../Data/feat_test")
 
-
-# Utility function to report best scores
-def report(results, n_top=15):
-    for i in range(1, n_top + 1):
-        candidates = np.flatnonzero(results['rank_test_score'] == i)
-        for candidate in candidates:
-            print("Model with rank: {0}".format(i))
-            print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
-                  results['mean_test_score'][candidate],
-                  results['std_test_score'][candidate]))
-            print("Parameters: {0}".format(results['params'][candidate]))
-            print("------------------------------")
-
-fold = 5
 ##################################  Neural Networks ##################################
 
 from keras.wrappers.scikit_learn import KerasClassifier
@@ -38,80 +24,92 @@ batch_size_ls =  [10, 25, 32, 50]
 optimizer_ls = ['adam', 'rmsprop']
 dropout_ls = [0.1,0.2,0.4]
 
-i=0
-dat_train = pd.read_csv('../Data/feat_train/' + file_name_train_list[i])
-dat_train.fillna(0, inplace=True)
-dat_test = pd.read_csv('../Data/feat_test/' + file_name_test_list[i])
-dat_test.isnull().values.any()
-dat_test.fillna(0, inplace=True)
 
-train_label = dat_train['_signal'].values
-test_label = dat_test['_signal'].values
+def data_prepare(dat_train,dat_test):
+    train_label = dat_train['_signal'].values
+    test_label = dat_test['_signal'].values
 
-y_train = np.array([int(item.split('_')[-3]) for item in train_label])
-X_train = dat_train.iloc[:, 1:].values
-y_test = np.array([int(item.split('_')[-3]) for item in test_label])
-X_test = dat_test.iloc[:, 1:].values
-Counter(y_train);
-Counter(y_test);
-# feature scaling
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+    y_train = np.array([int(item.split('_')[-3]) for item in train_label])
+    X_train = dat_train.iloc[:, 1:].values
+    y_test = np.array([int(item.split('_')[-3]) for item in test_label])
+    X_test = dat_test.iloc[:, 1:].values
+    Counter(y_train);
+    Counter(y_test);
+    # feature scaling
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
 
 
-y_train_bin = to_categorical(y_train)
-y_test_bin = to_categorical(y_test)
-max_acc = 0
-for batch_size in batch_size_ls:
-    for optimizer in optimizer_ls:
-        for dropout in dropout_ls:
+    y_train_bin = to_categorical(y_train)
+    y_test_bin = to_categorical(y_test)
 
-            clf = Sequential()
-            clf.add(Dense(units = 100, kernel_initializer = 'normal', activation = 'relu', input_dim = 92))
-            clf.add(Dropout(dropout))
-            clf.add(Dense(units = 50, kernel_initializer = 'normal', activation = 'relu'))
-            clf.add(Dropout(dropout))
-            clf.add(Dense(units = len(np.unique(y_train)) + 1, kernel_initializer = 'normal',activation='softmax'))
-            clf.compile(optimizer = optimizer, loss = 'categorical_crossentropy',metrics=[metrics.categorical_accuracy])
+    return X_train, X_test, y_train, y_test, y_train_bin, y_test_bin
 
-            clf.fit(X_train, y_train_bin, batch_size=batch_size, epochs=100)
-            eval = clf.evaluate(X_train,y_train_bin)
-        if eval[1] > max_acc:
-            max_acc = eval[1]
-            param_est = (batch_size,dropout,optimizer)
-            print (max_acc, param_est)
+n_epoch = 100
+####### model training and tuning
+def training_model(X_train, y_train_bin):
+    max_acc = 0
+    for batch_size in batch_size_ls:
+        for optimizer in optimizer_ls:
+            for dropout in dropout_ls:
+
+                clf = Sequential()
+                clf.add(Dense(units = 100, kernel_initializer = 'normal', activation = 'relu', input_dim = 92))
+                clf.add(Dropout(dropout))
+                clf.add(Dense(units = 50, kernel_initializer = 'normal', activation = 'relu'))
+                clf.add(Dropout(dropout))
+                clf.add(Dense(units = y_train_bin.shape[1], kernel_initializer = 'normal',activation='softmax'))
+                clf.compile(optimizer = optimizer, loss = 'categorical_crossentropy',metrics=[metrics.categorical_accuracy])
+
+                clf.fit(X_train, y_train_bin, batch_size=batch_size, verbose=False, epochs=n_epoch)
+                eval = clf.evaluate(X_train,y_train_bin)
+                if eval[1] > max_acc:
+                    max_acc = eval[1]
+                    param = (batch_size,dropout,optimizer)
+
+    return max_acc, param
+
 
 
 ####### classification and prediction on test data
-def classification_task(PARAM):
+def classification_task(PARAM,X_train,X_test,y_train_bin,y_test_bin):
     clf = Sequential()
     clf.add(Dense(units=100, kernel_initializer='normal', activation='relu', input_dim=92))
     clf.add(Dropout(PARAM[1]))
     clf.add(Dense(units=50, kernel_initializer='normal', activation='relu'))
     clf.add(Dropout(PARAM[1]))
-    clf.add(Dense(units=len(np.unique(y_train)) + 1, kernel_initializer='normal', activation='softmax'))
+    clf.add(Dense(units=y_train_bin.shape[1], kernel_initializer='normal', activation='softmax'))
     clf.compile(optimizer=PARAM[2], loss='categorical_crossentropy', metrics=[metrics.categorical_accuracy])
 
-    clf.fit(X_train, y_train_bin, batch_size=PARAM[0], epochs=100)
+    clf.fit(X_train, y_train_bin, batch_size=PARAM[0], verbose=False, epochs=n_epoch)
     my_accuracy = clf.evaluate(X_test,y_test_bin)[1]
 
     return my_accuracy
 
-classification_task(param_est)
-
-acc_list = []
-### Output to txt file
 orig_stdout = sys.stdout
-f = open('../Output/accuracies.txt', 'w')
+f = open('../Output/accuracies_ann.txt', 'w')
 sys.stdout = f
 
 for i in range(2):
+    dat_train = pd.read_csv('../Data/feat_train/' + file_name_train_list[i])
+    dat_train.fillna(0, inplace=True)
+    dat_test = pd.read_csv('../Data/feat_test/' + file_name_test_list[i])
+    dat_test.isnull().values.any()
+    dat_test.fillna(0, inplace=True)
+    X_train, X_test, y_train, y_test, y_train_bin, y_test_bin = data_prepare(dat_train, dat_test)
+    train_acc, train_param = training_model(X_train, y_train_bin)
 
-
+    accuracy = classification_task(train_param, X_train, X_test, y_train_bin, y_test_bin)
+    print(file_name_train_list[i], np.round(accuracy, 3), '\n', file=f, flush=True)
 
 sys.stdout = orig_stdout
 f.close()
+
+
+
+
+
 
 
 
